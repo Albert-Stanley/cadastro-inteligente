0f80d52d45de0eee945c535b55cd9a6cc207b90a
Albert Stanley <153841479+Albert-Stanley@users.noreply.github.com>
2025-02-04 00:36:06 -0300

    fixing route error

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Cadastro from "./pages/Cadastro";
-import DadosCadastrados from "./pages/DadosCadastrados";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/dados" element={<DadosCadastrados />} />
       </Routes>
     </Router>
   );
